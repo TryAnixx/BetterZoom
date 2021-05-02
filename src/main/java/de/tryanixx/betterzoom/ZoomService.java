55a703d11dfca4b2abb3e3f807d718ac603c12b1
Timo <raiddzn73@gmail.com>
2021-05-02 16:34:51 +0200

    Update ZoomService.java

--- src/main/java/de/tryanixx/betterzoom/ZoomService.java
+++ src/main/java/de/tryanixx/betterzoom/ZoomService.java
@@ -26,6 +26,7 @@ public class ZoomService extends LabyModAddon {
     @Override
     public void onEnable() {
         api.registerForgeListener(this);
+        System.out.println("BetterZoom started");
     }
 
     @Override

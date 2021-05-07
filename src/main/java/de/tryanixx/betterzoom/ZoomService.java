b6bc1fad2431a56514844b26bcc2dced01d7d6ca
Timo <raiddzn73@gmail.com>
2021-05-07 13:42:02 +0200

    added nohand

--- src/main/java/de/tryanixx/betterzoom/ZoomService.java
+++ src/main/java/de/tryanixx/betterzoom/ZoomService.java
@@ -6,11 +6,15 @@ import net.labymod.settings.elements.KeyElement;
 import net.labymod.settings.elements.SettingsElement;
 import net.labymod.utils.Material;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.EntityRenderer;
+import net.minecraft.client.renderer.entity.RenderPlayer;
 import net.minecraft.client.settings.GameSettings;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
+
+import java.lang.reflect.Field;
 import java.util.List;
 
 public class ZoomService extends LabyModAddon {
@@ -23,9 +27,18 @@ public class ZoomService extends LabyModAddon {
     private int hotbarslot;
     private boolean hotbarslotstate;
 
+    private Field fieldHand;
+
     @Override
     public void onEnable() {
         api.registerForgeListener(this);
+        try {
+            fieldHand = Minecraft.getMinecraft().entityRenderer.getClass().getDeclaredField("renderHand");
+            fieldHand.setAccessible(true);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
         System.out.println("BetterZoom started");
     }
 
@@ -53,12 +66,16 @@ public class ZoomService extends LabyModAddon {
                 zoomFactor--;
             }
         }
-
         boolean keyDown = Keyboard.isKeyDown(this.key);
         if (keyDown) {
             if (!this.lastKeyDown) {
                 this.lastKeyDown = true;
                 this.prevZoom = Minecraft.getMinecraft().gameSettings.getOptionFloatValue(GameSettings.Options.FOV);
+                try {
+                    fieldHand.set(Minecraft.getMinecraft().entityRenderer, false);
+                } catch (IllegalAccessException illegalAccessException) {
+                    illegalAccessException.printStackTrace();
+                }
             }
             (Minecraft.getMinecraft()).gameSettings.smoothCamera = true;
             Minecraft.getMinecraft().gameSettings.setOptionFloatValue(GameSettings.Options.FOV, 20F / zoomFactor);
@@ -67,6 +84,11 @@ public class ZoomService extends LabyModAddon {
                 hotbarslotstate = true;
             }
         } else if (this.lastKeyDown) {
+            try {
+                fieldHand.set(Minecraft.getMinecraft().entityRenderer, true);
+            } catch (IllegalAccessException illegalAccessException) {
+                illegalAccessException.printStackTrace();
+            }
             this.lastKeyDown = false;
             (Minecraft.getMinecraft()).gameSettings.smoothCamera = false;
             Minecraft.getMinecraft().gameSettings.setOptionFloatValue(GameSettings.Options.FOV, prevZoom);
